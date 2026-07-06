8d42e06ca63a8a30d944b9a57e3449d3a85baa63
HeroWings <maximilian-vogel1@gmx.de>
2026-07-06 19:43:35 +0200

    fix: remove treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
-	branch = 'master', 
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup{
-			auto_install = true,
-			highlight = { enable = true },
-			indent = {enable = true },
-			ensure_installed = { 
-				"c", "lua", "vim","typst", "vimdoc", "query", "markdown", "markdown_inline" },
-		}
-	end,
-
-	lazy = false, 
-	build = ":TSUpdate", 
+	-- "nvim-treesitter/nvim-treesitter", 
+	-- branch = 'main', 
+	-- config = function(_, _opts)
+	-- 	require("nvim-treesitter.config").setup{
+	-- 		auto_install = true,
+	-- 		highlight = { enable = true },
+	-- 		indent = {enable = true },
+	-- 		ensure_installed = { 
+	-- 			"c", "lua", "vim","typst", "vimdoc", "query", "markdown", "markdown_inline" },
+	-- 	}
+	-- end,
+	--
+	-- lazy = false, 
+	-- build = ":TSUpdate", 
 }
 
